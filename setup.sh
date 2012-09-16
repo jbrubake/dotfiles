2ca3d920c2d84a39c7d3f604dd15866dbe9664f8
Jeremy Brubaker <jbru362@gmail.com>
2012-09-16 10:27:41 -0400

    setup.sh no longer installs dotfiles/bin

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ F=`pwd |sed -e "s#$HOME/\?##"`
 
 for P in *
 do
-    # skip setup, README.md and bin
+    # skip setup and README.md
     if [ "$P" = "setup.sh" ]; then continue; fi
     if [ "$P" = "README.md" ]; then continue; fi
-    if [ "$P" == "bin" ]; then continue; fi
+
     # ensure permissions
     chmod -R o-rwx,g-rwx $P
 
@@ -31,28 +31,3 @@ do
     ln -v -s "$F/$P" "$HOME/.$P"
 done
 
-# hack for bin
-cd bin
-for P in *
-do
-    # ensure permissions
-    chmod -R o-rwx,g-rwx $P
-
-    # skip existing links
-    if [ -h "$HOME/bin/$P" ]; then continue; fi
-
-    # move existing dir out of the way
-    if [ -e "$HOME/bin$P" ]; then
-        if [ -e "$HOME/bin/$P~" ]; then
-            echo "want to override $HOME/bin/$P~ but backup exists"
-            continue;
-        fi
-
-        echo -n "Backup "
-        mv -v "$HOME/bin/$P" "$HOME/bin/$P~"
-    fi
-
-    # create link
-    echo -n "Link "
-    ln -v -s "../$F/bin/$P" "$HOME/bin/$P"
-done
