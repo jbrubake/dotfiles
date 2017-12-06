04dac638e1a06672a0079b2baf916a37978770d0
Jeremy Brubaker <jbru362@gmail.com>
2017-12-05 21:37:32 -0500

    Use ln(1) options to create backups

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@
 # a '.' prepended.
 #
 #
-# TODO: Call via 'make install'
 # TODO: Only install if dest is older
 show_help() {
     cat <<EOF
@@ -44,7 +43,7 @@ IFS='
 ##
 ## Command line options
 ##
-FORCE=              # empty means do not overwrite files
+BACKUP=-b           # backup existing files
 VERBOSE=            # empty means not verbose
 DESTDIR="$HOME"     # where to install everything
 HOST=`hostname`
@@ -54,7 +53,7 @@ HOSTIGNORE=$IGNOREFILE.$HOST    # host-specific ignore file
 while getopts "n:fd:vh" opt; do
     case $opt in
         n) HOST=$OPTARG; HOSTIGNORE="$IGNOREFILE.$h" ;;
-        f) FORCE='-f' ;;
+        f) BACKUP='-f' ;;
         d) DESTDIR=$OPTARG ;;
         v) VERBOSE='-v' ;;
         h) show_help ;;
@@ -73,12 +72,10 @@ DOTPATH=`pwd |sed -e "s#$DESTDIR/\?##"` # make relative links
 
 # Action happens here, following these rules:
 #
-# - skip files in .ignore
-# - skip files in .ignore.<host> if hostname = <host>
+# - skip files in .ignore and .ignore.<host>
 # - if -f was *not* specified
-# -     Skip existing links
+# -     Skip if backup exists
 # -     Backup existing files
-# -     Skip existing files if backup exists
 # - else -f *was* specified
 # -     Overwrite existing links and files
 #
@@ -89,30 +86,14 @@ do
         test $f = $p && continue 2 # continue OUTER LOOP
     done
 
-    # if file exists and -f not specified, make backups
-    if test -e "$DESTDIR/.$f" || test -L "$DESTDIR/.$f" &&
-        test -z $FORCE
+    # skip if -f not specified and backup exists
+    if test $BACKUP = '-b' && test -e "$DESTDIR/$f~"
     then
-        # skip existing links
-        if test -L "$DESTDIR/.$f"
-        then
-            test -n $VERBOSE &&
-                echo "Skipping existing link .$f" >&2
-            continue
-        fi
-
-        # backup existing files
-        # FIXME: This doesn't work if $tgt contains a path
-        if test -e "$DESTDIR/__$f"
-        then
-            echo "Backup __$f already exists. Skipping" >&2
-            continue
-        else
-            mv $VERBOSE $FORCE "$DESTDIR/.$f" "$DESTDIR/__$f"
-        fi
+        echo "Backup $f~ already exists. Skipping" >&2
+        continue
     fi
 
     # make links
-    # XXX: Is there an 'install' equivalent?
-    ln $VERBOSE -s $FORCE "$DOTPATH/$f" "$DESTDIR/.$f"
+    ln $VERBOSE $BACKUP "$DOTPATH/$f" "$DESTDIR/.$f" 
 done
+
