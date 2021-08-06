4c02b680a086ec3f04c30053c44fbe95610291ab
Jeremy Brubaker <jbru362@gmail.com>
2021-08-06 15:30:23 -0400

    Add tmux rpg-cli plugin

--- tmux/status_right.sh
+++ tmux/status_right.sh
@@ -4,8 +4,8 @@ PLUGINS=$(tmux show-option -gqv @plugin_dir)
 source $PLUGINS/utils/plugins.sh
 
 printf "#[fg=white,bg=black]"
-printf " #[fg=yellow]%s" "$(plugin mem_usage)"
-printf " %s" "$(plugin load)"
+printf " #[fg=magenta]Hero HP: %s%%" "$(plugin rpg)"
+printf " #[fg=white]| #[fg=yellow]%s %s #[fg=white]|" "$(plugin mem_usage)" "$(plugin load)"
 printf " #[fg=green,bold]%s" "$(plugin clock)"
 printf "#[fg=white,none]%s" "$(plugin weather)"
 printf " "
