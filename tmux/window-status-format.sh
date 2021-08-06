54c4790a5f6cfd2df0194193410ea64c601d7ea2
Jeremy Brubaker <jbru362@gmail.com>
2021-08-06 15:07:28 -0400

    Change tmux window-status colors

--- tmux/window-status-format.sh
+++ tmux/window-status-format.sh
@@ -4,12 +4,12 @@ bar="black"
 
 case "$1" in
     *Z*) fg=yellow;   bg=$bar; other=none ;; # zoomed
-    *\**) fg=blue;   bg=$bar; other=bold ;; # current
+    *\**) fg=green;   bg=$bar; other=bold ;; # current
     *#*) fg=red;    bg=$bar; other=none ;; # activity
     *!*) fg=yellow; bg=$bar; other=none ;; # bell
     *-*) fg=white;  bg=$bar; other=none ;; # last
-    *~*) fg=blue;   bg=$bar; other=none ;; # silent
-    *M*) fg=blue;   bg=$bar; other=none ;; # marked
+    *~*) fg=green;   bg=$bar; other=none ;; # silent
+    *M*) fg=green;   bg=$bar; other=none ;; # marked
     *)   fg=grey;   bg=$bar; other=none ;; # other
 esac
 
