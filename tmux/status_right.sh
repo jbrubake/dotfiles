c26eaba4a27f314aa860351bb055184f0ce7ec10
Jeremy Brubaker <jbru362@gmail.com>
2023-01-24 10:46:41 -0500

    Configure tmux statusbar

--- tmux/status_right.sh
+++ tmux/status_right.sh
@@ -4,8 +4,8 @@ PLUGINS=$(tmux show-option -gqv @plugin_dir)
 source $PLUGINS/utils/plugins.sh
 
 printf "#[fg=white,bg=black]"
-printf " #[fg=yellow]%s %s #[fg=white]| %s #[fg=white]" "$(emojify :computer:)" "$(plugin mem_usage)" "$(plugin load)"
-printf " #[fg=green,bold]%s %s" "$(emojify :calendar:)" "$(plugin clock)"
-printf "#[fg=white,none]%s" "$(plugin weather)"
+printf " #[fg=yellow]%s %s #[fg=white]| %s#[fg=white]" "$(emojify :computer:)" "$(plugin mem_usage)" "$(plugin load)"
+printf " |#[fg=green,bold] %s %s" "$(emojify :calendar:)" "$(plugin clock)"
+printf "#[fg=white,none] | %s" "$(plugin weather)"
 printf " "
 
