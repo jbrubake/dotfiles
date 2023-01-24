96d72dbf03c855ee9dc2fa5efa68279b12e4f3cf
Jeremy Brubaker <jbru362@gmail.com>
2023-01-24 10:41:19 -0500

    Cleanup and improve keymaps.sh
    - Set `Compose` using `setxkbmap`
    - Set `L_Ctrl` using `xcape`
    - Start `sxhkd`

--- etc/init.d/keymaps.sh
+++ etc/init.d/keymaps.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-# -layout us                 : US keyboard
-# -variant altgr-intl        : enable 3rd level symbols
-# -option                    : clear options
-# -option lv3:switch         : Control_R = AltGr
-# -option keypad:pointerkeys : enable mousekeys
+# -layout us                          : US keyboard
+# -variant altgr-intl                 : enable 3rd level symbols
+# -option                             : clear options
+# -option keypad:pointerkeys          : enable mousekeys
+# -option shift:both_caps_lock_cancel : L_Shift+R_Shift = CapsLock; any Shift cancels
+# -option compose:rctrl               : R_Ctrl = Compose
+# -option ctrl:nocaps                 : CapsLock = L_Ctrl
 setxkbmap -layout us -variant altgr-intl \
     -option \
     -option keypad:pointerkeys \
-    -option shift:both_capslock_cancel
+    -option shift:both_capslock_cancel \
+    -option compose:rctrl \
+    -option ctrl:nocaps
 
 # Keep xmodmap stuff here so all the keyboard
 # configuration is in a single file
+
+# 37 (Left Ctrl) = ISO_level3_Shift
+# 108 (Right Alt) = Left Alt
+#
 xmodmap -e "clear lock"
 xmodmap -e "clear control"
 xmodmap -e "clear mod1"
@@ -19,8 +27,6 @@ xmodmap -e "clear mod2"
 xmodmap -e "clear mod3"
 xmodmap -e "clear mod4"
 xmodmap -e "clear mod5"
-xmodmap -e "keycode 66 = Control_L"
-xmodmap -e "keycode 105 = Multi_key"
 xmodmap -e "keycode 37 = ISO_Level3_Shift"
 xmodmap -e "keycode 108 = Alt_L"
 xmodmap -e "add control = Control_L"
@@ -28,9 +34,10 @@ xmodmap -e "add mod1 = Alt_L Meta_L"
 xmodmap -e "add mod2 = Num_Lock"
 xmodmap -e "add mod4 = Super_L"
 
-# Control_L (Caps_Lock key) = Escape by itself
 if command -v xcape 2>&1 >/dev/null; then
-    xcape -e 'Control_L=Escape'
-    xcape -e 'Super_L=Super_R'
+    xcape -e 'Control_L=Escape' # Control_L (Caps_Lock key) = Escape by itself
+    xcape -e 'Super_L=Super_R' # Super_L = Super_R by itself
 fi
+
+command -v sxhkd >/dev/null && sxhkd &
 
