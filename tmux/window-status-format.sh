acf5049ee0c23ba2a94c25dd1e86993c4c74cf6e
Jeremy Brubaker <jbru362@gmail.com>
2024-08-10 23:27:04 -0400

    Add pre/post strings to tmux window list items

--- tmux/window-status-format.sh
+++ tmux/window-status-format.sh
@@ -3,14 +3,15 @@
 bar="black"
 
 case "$1" in
-    *Z*) fg=yellow;   bg=$bar; other=none ;; # zoomed
-    *\**) fg=green;   bg=$bar; other=bold ;; # current
-    *#*) fg=red;    bg=$bar; other=none ;; # activity
-    *!*) fg=yellow; bg=$bar; other=none ;; # bell
-    *-*) fg=white;  bg=$bar; other=none ;; # last
-    *~*) fg=green;   bg=$bar; other=none ;; # silent
-    *M*) fg=green;   bg=$bar; other=none ;; # marked
-    *)   fg=grey;   bg=$bar; other=none ;; # other
+    *Z*)  fg=yellow; bg=$bar; other=none; pre='['; post=']' ;; # zoomed
+    *\**) fg=green;  bg=$bar; other=bold; pre=''; post='' ;; # current
+    *#*)  fg=red;    bg=$bar; other=none; pre='['; post=']' ;; # activity
+    *!*)  fg=yellow; bg=$bar; other=none; pre='['; post=']' ;; # bell
+    *-*)  fg=white;  bg=$bar; other=none; pre='['; post=']' ;; # last
+    *~*)  fg=green;  bg=$bar; other=none; pre='['; post=']' ;; # silent
+    *M*)  fg=green;  bg=$bar; other=none; pre='['; post=']' ;; # marked
+    *)    fg=grey;   bg=$bar; other=none; pre='['; post=']' ;; # other
 esac
 
-printf "#[fg=$fg,bg=$bg,$other]#I#F #W"
+printf "#[fg=%s,bg=%s,%s]%s#I#F #W%s" "$fg" "$bg" "$other" "$pre" "$post"
+
